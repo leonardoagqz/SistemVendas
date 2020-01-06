94665492e8c54d35715d8e08de9623de86de093c
leonardo <leonardoagqz@gmail.com>
2020-01-06 19:31:01 -0200

    adicionado componente radiogroup na consulta de produtos

--- Fontes/U_produtos.pas
+++ Fontes/U_produtos.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.Grids, Vcl.DBGrids,
   Vcl.ComCtrls, Vcl.StdCtrls, Vcl.Imaging.jpeg, Vcl.ExtCtrls,
-  Vcl.Imaging.pngimage;
+  Vcl.Imaging.pngimage, Vcl.Buttons;
 
 type
   TF_produtos = class(TForm)
@@ -17,13 +17,16 @@ type
     dbg_produtos: TDBGrid;
     edt_busca: TEdit;
     Label1: TLabel;
-    img_buscarprodutos: TImage;
     Image1: TImage;
+    SpeedButton_buscarprodutos: TSpeedButton;
+    Image2: TImage;
+    rg_buscar: TRadioGroup;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure edt_buscaKeyPress(Sender: TObject; var Key: Char);
     procedure edt_buscaChange(Sender: TObject);
     procedure img_buscarprodutosClick(Sender: TObject);
+    procedure SpeedButton_buscarprodutosClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -59,24 +62,29 @@ end;
 
 procedure TF_produtos.edt_buscaKeyPress(Sender: TObject; var Key: Char);
 begin
-  
-  if Key = #13 then
-    begin
-  
-    with dm.SQL_produtos do
-         begin
-           Close;
-           SQL.Clear;
-           SQL.Add('select * from produtos');
-           SQL.Add('where pro_nome like :nome');
-           ParamByName('nome').Value := edt_busca.Text + '%';
-           Open;
 
-           if RecordCount = 0 then
-           ShowMessage('Produto não encontrado!');
-         end;
-    
-      end;
+  if Key = #13 then
+      begin
+
+        with dm.SQL_produtos do
+               begin
+                 Close;
+                 SQL.Clear;
+                 SQL.Add('select * from produtos');
+                    case rg_buscar.ItemIndex of
+                    0 : SQL.Add('where pro_nome like :nome');
+                    1 : SQL.Add('where pro_barras like :nome');
+
+                    end;
+
+                 ParamByName('nome').Value := edt_busca.Text + '%';
+                 Open;
+
+                 if RecordCount = 0 then
+                 ShowMessage('Produto não encontrado!');
+               end;
+
+          end;
   
 
 end;
@@ -88,9 +96,8 @@ end;
 
 procedure TF_produtos.FormCreate(Sender: TObject);
 begin
-  dm.SQL_produtos.Open;
-  edt_busca.SetFocus;
-  
+  //dm.SQL_produtos.Open;
+
 end;
 
 procedure TF_produtos.img_buscarprodutosClick(Sender: TObject);
@@ -109,7 +116,27 @@ begin
 
            if edt_busca.Text = '' then
            ShowMessage('Campo vazio!');
-           
+
+         end;
+end;
+
+procedure TF_produtos.SpeedButton_buscarprodutosClick(Sender: TObject);
+begin
+  with dm.SQL_produtos do
+         begin
+           Close;
+           SQL.Clear;
+           SQL.Add('select * from produtos');
+           SQL.Add('where pro_nome like :nome');
+           ParamByName('nome').Value := edt_busca.Text + '%';
+           Open;
+
+           if RecordCount = 0 then
+           ShowMessage('Produto não encontrado!');
+
+           if edt_busca.Text = '' then
+           ShowMessage('Campo vazio!');
+
          end;
 end;
 
